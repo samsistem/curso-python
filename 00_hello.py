1d9a0d041c3385aa40127e36ebb673eb472b1644
samsistem <samsistem@hotmail.com>
2025-02-03 23:09:15 +0100

    primera clase

---
+++ 00_hello.py
@@ -0,0 +1,12 @@
+# Esto es un comentario: Elooooo
+# Otro comentario
+"""
+Comentario en varias lineas
+
+"""
+print ("Hola mundo")
+print (type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print (type(1.5)) # Tipo 'float'
+print (type(3 + 1j) ) # Tipo 'complex'
+print(type (True)) # Tipo 'bool'
