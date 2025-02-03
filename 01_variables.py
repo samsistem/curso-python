85bc7a884dc0ca0516d96fc7314b1d0bda3ffb80
samsistem <samsistem@hotmail.com>
2025-02-04 00:04:54 +0100

    curso python

---
+++ 01_variables.py
@@ -0,0 +1,22 @@
+#variables
+
+my_string_variable = "mi variable"
+print (my_string_variable)
+
+my_int_variable = 5
+print (my_int_variable)
+
+my_bool_variable = False
+print (my_bool_variable)
+
+#concatenar variables
+print (my_bool_variable, my_int_variable, my_string_variable)
+print ("Este es el valor de: ", my_bool_variable)
+
+# Algunas funciones del sistema
+print (len(my_string_variable))
+
+
+
+
+
