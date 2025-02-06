59c58258d88de9536742cba2077886f0c801bb78
samsistem <samsistem@hotmail.com>
2025-02-07 00:12:26 +0100

    curso

---
+++ ejercicio2.py
@@ -0,0 +1,32 @@
+name = ("Sam sistem")
+age = (44)
+height = (1,78)
+print(name)
+print( age)
+print(height)
+
+age = (str (44))
+print (("Tengo: "), age)
+print (type(age))
+is_student = False
+print ("eres estudiante?", is_student)
+print (len (name))
+
+nombre, apellidos, ciudad = ("Sam", "sistem", "agtun")
+print(nombre, apellidos, ciudad)
+
+color = input("Dime un color: ")
+print(("Color elegido: "), color)
+fruit = "Pera"
+print (fruit)
+fruit = "Manzana"
+print(fruit)
+price = (13.2)
+print(int(price))
+address = "calle caspe 22"
+address_len= (len(address))
+print(address_len)
+
+phone=(666)
+phone=(444)
+print(type(phone))
