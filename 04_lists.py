18d269b739570c9fce8a29a824dca8571607b6ce
samsistem <samsistem@hotmail.com>
2025-02-11 11:21:27 +0100

    curso

---
+++ 04_lists.py
@@ -0,0 +1,14 @@
+#listas
+
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [34, 3, 56, 34, 23, 45, 67, 78, 89, 90]
+print(my_list)
+print(len(my_list))
+
+my_other_list = [35, 1.77, "Hello", "eloooo"]
+print(my_other_list)
