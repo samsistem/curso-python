77b5898dc9d9e0d0b356e63d5d718d6d00abd194
samsistem <samsistem@hotmail.com>
2025-02-17 12:50:45 +0100

    libro

---
+++ libro/Capitulo 4/first_number.py
@@ -0,0 +1,14 @@
+for value in range(1,6):
+    print(value)
+
+numbers = list(range(1,6))
+print(numbers)
+
+even_numbers = list(range(2,11,2))
+print(even_numbers)
+
+squares = []
+for value in range(1,11):
+    squares.append(value**2)
+    
+print(squares)
---
+++ libro/Capitulo 4/magicians.py
@@ -0,0 +1,12 @@
+lista = ["coche", "moto", "avion", "barco"]
+for lista in lista:
+    print(f"Me gustan los: {lista.title()}")
+    
+    print(f"Estamos deseando ver los trucos de magia de: {lista.title()}\n")
+print("Gracias por venir!\n")
+
+lista = ["coche", "moto", "avion", "barco"]
+print("me gustan los siguientes medios de transporte:\n")
+for lista in lista:
+    print(lista.title())
+print("\nSon todos increible!")
