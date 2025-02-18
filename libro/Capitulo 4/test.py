b0acf27a2a7b10858ce38188759f8ab602dac09b
samsistem <samsistem@hotmail.com>
2025-02-18 12:19:17 +0100

    libro

---
+++ libro/Capitulo 4/test.py
@@ -0,0 +1,5 @@
+lista = ['chopiaa', 'lanias', 'bubias']
+message = "Estos son mis colegas:"
+print(f"{message}\n")
+for lista in lista:
+    print(lista.title())
---
+++ players.py
@@ -0,0 +1,2 @@
+players = ['charlie', 'martina', 'michel', 'florence', 'eli']
+print(players[0:3])
